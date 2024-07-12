b74f51858f6b9f48d96b3147ac8d3e16319d07c3
Martin Toman <25009432+tinybeachthor@users.noreply.github.com>
2024-07-11 19:22:09 -0500

    Nix flake init rust

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    with flake-utils.lib;
+    eachSystem [ system.x86_64-linux ] (system: let
+      pkgs = (import nixpkgs {
+        inherit system;
+        overlays = [
+          rust-overlay.overlays.default
+        ];
+      });
+    in {
+      devShells.default = let
+        my-rust = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [
+            "rust-src"
+            "rls-preview"
+          ];
+          targets = [
+            "x86_64-unknown-linux-gnu"
+          ];
+        };
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [
+          git
+
+          my-rust
+        ];
+      };
+    });
+}

caeb19165fe3f6f09bef448182922f699d57ca87
Martin Toman <25009432+tinybeachthor@users.noreply.github.com>
2024-07-14 18:28:29 -0500

    Use typed-langid for Template

--- i1-templates-8n/examples/manual_impl.rs
+++ i1-templates-8n/examples/manual_impl.rs
@@ -1,13 +1,13 @@
-use i1_templates_8n::Template;
+use i1_templates_8n::{Template, typed_langid};
 
 struct HelloWorld {
     hello: String,
     world: String,
 }
-impl Template for HelloWorld {
+impl Template<typed_langid::En> for HelloWorld {
     type Output = String;
 
-    fn render(&self) -> Self::Output {
+    fn render(&self, _lang: typed_langid::En) -> Self::Output {
         let HelloWorld {
             hello,
             world,
@@ -15,12 +15,32 @@ impl Template for HelloWorld {
         std::format!("{hello} {world}!")
     }
 }
+impl Template<typed_langid::Fr> for HelloWorld {
+    type Output = String;
+
+    fn render(&self, _lang: typed_langid::Fr) -> Self::Output {
+        let HelloWorld {
+            hello,
+            world,
+        } = self;
+        std::format!("{world} {hello}!")
+    }
+}
 
 fn main() {
-    let x = HelloWorld {
+    let template = HelloWorld {
         hello: "Howdy".to_string(),
         world: "Galaxy".to_string(),
     };
-    println!("{:?}", x.render());
+
+    render(template)
 }
 
+fn render<T>(template: T)
+where
+    T: Template<typed_langid::En, Output = String>,
+    T: Template<typed_langid::Fr, Output = String>,
+{
+    println!("{:?}", template.render(typed_langid::En));
+    println!("{:?}", template.render(typed_langid::Fr));
+}
--- i1-templates-8n/src/lib.rs
+++ i1-templates-8n/src/lib.rs
@@ -1,16 +1,17 @@
+pub use typed_langid::{self, LangId, LangIdUnion};
 pub use i1_templates_8n_derive::Template;
 
-pub trait Template {
+pub trait Template<L: LangId> {
     type Output;
 
-    fn render(&self) -> Self::Output;
+    fn render(&self, lang: L) -> Self::Output;
 }
 
-impl<T: Template + ?Sized> Template for &T {
+impl<T: Template<L> + ?Sized, L: LangId> Template<L> for &T {
     type Output = T::Output;
 
     #[inline]
-    fn render(&self) -> Self::Output {
-        T::render(self)
+    fn render(&self, lang: L) -> Self::Output {
+        T::render(self, lang)
     }
 }
